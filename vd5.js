07a208e2027b3b9e64fc4aa4aa71464dc3dbc120
haikhoi <hai.le@esec.vn>
2023-03-17 16:10:39 +0700

    chap7 vd7

--- vd5.js
+++
@@ -1,9 +0,0 @@
-let a = `i'm string`
-let x = 5, y = 6
-
-console.log(`vd thu 1' voi backstick: `, a == x)
-
-let d = '5'
-let e = +d  // number(d)
-console.log('e value:', e, 'type:', typeof e)
-console.log(x === e, 'x: ', typeof x)
